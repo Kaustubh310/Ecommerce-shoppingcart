1a471b253c5d28ce0fbe458ba9c4a7e221671fdf
Kaustubh <bandkarkaustubh03@gmail.com>
2025-01-03 16:30:44 +0530

    products fetch

--- ecomm/products/views.py
+++ ecomm/products/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from products.models import Contact,Product
-
 from django.contrib import messages
+from math import ceil
+
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+
+    allProds = []
+    catprods = Product.object.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cats in cats:
+        prod = Product.object.filter(category=cats)
+        n=len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    params = {'allProds': allProds}
+
+    return render(request,"index.html",params)
 
 def contact(request):
     if request.method=="POST":
